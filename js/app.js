1046de06dae1dfc30a06131c558a4dd169019e24
Janke-Rall <u24571238@tuks.co.za>
2026-08-09 12:04:11 +0200

    updating app

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 const STARBUD_DB_KEY = 'starbud_users_v1';
 const STARBUD_SESSION_KEY = 'starbud_session_v1';
 
+function StarbudGetUsers() {
+  try {
+    return JSON.parse(localStorage.getItem(STARBUD_DB_KEY)) || [];
+  } catch (e) {
+    return [];
+  }
+}
